b08dc08f13da7952a462751748101cd5aa3e6a6a
Ayush Patel <ayushpatel95@gmail.com>
2025-10-01 22:03:38 -0400

    Adds font family, and base setup refactor.

--- Events-Lineup-List/app/src/main/java/com/example/eventslineuplist/MainActivity.kt
+++ Events-Lineup-List/app/src/main/java/com/example/eventslineuplist/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,7 +21,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             EventsLineupListTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                        .background(MaterialTheme.colorScheme.surface),
+                ) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
--- Events-Lineup-List/app/src/main/java/com/example/eventslineuplist/ui/theme/Theme.kt
+++ Events-Lineup-List/app/src/main/java/com/example/eventslineuplist/ui/theme/Theme.kt
@@ -14,7 +14,8 @@ import androidx.compose.ui.platform.LocalContext
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
+    surface = Color(0xFFFFFFF0),
 )
 
 private val LightColorScheme = lightColorScheme(
