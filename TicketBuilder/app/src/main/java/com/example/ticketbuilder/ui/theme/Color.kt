09113d088fd386d4df7ca609ecb992f2e2bc3662
Ayush Patel <ayushpatel95@gmail.com>
2025-10-02 23:19:39 -0400

    Base setup config.

---
+++ TicketBuilder/app/src/main/java/com/example/ticketbuilder/ui/theme/Color.kt
@@ -0,0 +1,18 @@
+package com.example.ticketbuilder.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val TextPrimary = Color(0xFF421E17)
+val TextSecondary = Color(0xFF786B68)
+val TextDisabled = Color(0xFFA99E9C)
+val Lime = Color(0xFFE0E270)
+val Surface = Color(0xFFFFFFF0)
+val SurfaceHigher = Color(0xFFFCF3E2)
